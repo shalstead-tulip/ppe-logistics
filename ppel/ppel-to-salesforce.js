ffb7321844fdee8f2a60094a89a468d8f3e0c6e3
Steven Halstead <steven.halstead@tulip.co>
2020-04-30 19:00:06 -0400

    pare down script

--- ppel/ppel-to-salesforce.js
+++ ppel/ppel-to-salesforce.js
@@ -1,37 +1,49 @@
 const jsforce = require("jsforce");
 const pg = require("pg");
-const Wal2JSONListener = require("node-wal2json");
 
 const { SECRETS } = require("./local-secrets.js");
+
+// Define global variable to store database client
+let dbClient;
+
+// Global variable for environment: either DEV or PROD
+const env = "DEV";
 
 // Salesforce Connection Info
 const username = "steven.halstead+maskson@tulip.co.tulipdev1";
 const password = SECRETS.SF_PWD;
 const securityToken = SECRETS.SF_SEC_TOK;
 
-// Postgres Connection Info
-const pgClient = new pg.Client({
-  host: "maskson-sfdc-dev-poc-db.cliunwsqnhh7.us-east-1.rds.amazonaws.com",
-  port: 5432,
-  user: "tulip",
-  password: SECRETS.PG_PWD,
-  database: "maskson_sfdc_dev",
-});
-
 const sfConn = new jsforce.Connection({
   // uncomment below for sandbox
   loginUrl: "https://test.salesforce.com",
 });
 
-const walOptions = {
-  "include-type-oids": 1,
-  "include-types": 1,
-};
+function getDBClient(pwd, env) {
+  if (env == "DEV") {
+    console.log("CONNECTING TO DEV DB");
+    dbClient = new pg.Client({
+      host: "maskson-sfdc-dev-poc-db.cliunwsqnhh7.us-east-1.rds.amazonaws.com",
+      port: 5432,
+      user: "tulip",
+      password: pwd,
+      database: "maskson_sfdc_dev",
+    });
+  } else if (env == "PROD") {
+    // console.log("CONNECTING TO PROD DB");
+    // dbClient = new pg.Client({
+    //   host: "three-d-corps-poc-db.cliunwsqnhh7.us-east-1.rds.amazonaws.com",
+    //   port: 5432,
+    //   user: "tulip",
+    //   password: pwd,
+    //   database: "three_d_corps",
+    // });
+  }
 
-const options = {
-  slotName: "test_slot",
-  timeout: 500,
-};
+  dbClient.connect();
+
+  return dbClient;
+}
 
 const syncMap = {
   demands: {
@@ -59,111 +71,192 @@ const syncMap = {
   },
 };
 
-// Single record update
-function updateRecordSFDC(change) {
-  const recordMap = syncMap[change.table];
-  const recordType = recordMap.object;
-  // TODO: fetch sdfc object ID for transport changes
-  // May need to restructure w/ Promises to do this
-  // Could get this either via PG query or SFDC api
-  // Cache these by fetching initial mapping of order IDs to SFDC IDs,
-  //  then add to as new orders created
-  var recordValues = {
-    Id: change.record.sfdc_object_id,
-  };
-
-  for (let [pgField, sfField] of Object.entries(recordMap.fields)) {
-    recordValues[sfField] = change.record[pgField];
-  }
-
-  console.log(
-    `--> Syncing Tulip '${change.table}' record to Salesforce '${recordType}'`
-  );
-  console.log(JSON.stringify(recordValues, null, 2));
-
-  sfConn.sobject(recordType).update(recordValues, function (err, ret) {
-    if (err || !ret.success) {
-      return console.error(err, ret);
+function listQueryFields(fieldMap) {
+  var fields = "";
+  for (let [key, value] of Object.entries(fieldMap)) {
+    if (key.includes("date")) {
+      fields = fields.concat(
+        `to_char(${key}, 'YYYY-MM-DD') || 'T' || to_char(${key}, 'HH:MI:SS.MSZ') AS ${value},\n`
+      );
+    } else {
+      fields = fields.concat(`${key} AS ${value},\n`);
     }
-    console.log("--> SFDC RECORD UPDATED");
+  }
+  fields = fields.slice(0, -2);
+  return fields;
+}
+
+function printResults(res) {
+  console.log(res);
+}
+
+function printRows(res) {
+  console.log(res.rows);
+  return res;
+}
+
+function printFields(res) {
+  res.fields.forEach(function (field) {
+    console.log(`"${field.name}": "",`);
   });
 }
-
-function filterEvent(change) {
-  var filters = {
-    kind: change.kind == "update",
-    schema: change.schema == "corps",
-    table: Object.keys(syncMap).includes(change.table),
-    org: change.record.org == "MasksOn",
-    // check that this isn't a change sycned from SFDC
-    sfdc_update: change.record.updated_at != change.record.updated_by_sfdc_at,
-    sfdc_object: change.record.sfdc_object_id != null,
-    failed: [],
-  };
-
-  for (let k of Object.keys(filters)) {
-    if (!filters[k]) {
-      filters.failed.push(k);
-    }
-  }
-  console.log("--> Filters: ", JSON.stringify(filters, null, 2));
-  return filters;
-}
-
-function processChangeSet(changeSet) {
-  for (change of changeSet) {
-    console.log("--> processing change:");
-    // console.log("Full record:\n", JSON.stringify(change, null, 2)); // log full change
-    console.log(
-      ` kind: ${change.kind}\n schema: ${change.schema}\n table: ${change.table}`
-    );
-
-    // key-value pair column names and values of record
-    change.record = {};
-    change.columnnames.forEach(
-      (key, i) => (change.record[key] = change.columnvalues[i])
-    );
-
-    // Event filters
-    var filters = filterEvent(change);
-
-    if (filters.failed.length == 0) {
-      console.log(">>> SYNC CHANGE TO SFDC");
-      updateRecordSFDC(change);
-    } else {
-      console.log(">>> IGNORED EVENT");
-      console.log("--> Did not pass filters:", filters.failed);
-    }
-  }
-}
-
-function handleInboundChangeSets(changeSets) {
-  if (changeSets.length > 0) {
-    console.log(">>> INBOUND CHANGESETS FROM POSTGRES");
-    //console.log(changeSets);
-    for (c of changeSets) {
-      processChangeSet(JSON.parse(c[2]).change);
-    }
-  }
-  wal2JSONListener.next();
-}
-
-// Set up WAL listener
-const wal2JSONListener = new Wal2JSONListener(pgClient, options, walOptions);
-
-wal2JSONListener.on("changes", handleInboundChangeSets);
-
-wal2JSONListener.on("error", function (err) {
-  console.log("err: ", err);
-});
 
 function failureCallback(error) {
   console.error("Error: " + error);
 }
 
-// "Main"
-sfConn
-  .login(username, password + securityToken)
-  .then((res) => console.log(">>> SFDC Connection Authenticated"))
-  .then((res) => wal2JSONListener.start())
-  .catch(failureCallback);
+/////////////////////
+// Run a bulk insert
+/////////////////////
+var test_instances_1 = [
+  {
+    Name: "bagel-1",
+    domain__c: "tulip.co",
+    deployment_id__c: "11101",
+    category__c: "SOIL",
+  },
+  {
+    Name: "bagel-2",
+    domain__c: "tulip.co",
+    deployment_id__c: "11102",
+    category__c: "SOIL",
+  },
+  {
+    Name: "bagel-3",
+    domain__c: "tulip.co",
+    deployment_id__c: "11103",
+    category__c: "SOIL",
+  },
+];
+
+let orderRecords;
+
+let jobResults;
+
+function instanceInfo(i) {
+  var record = instanceRecords[i];
+  return `${record.deployment_id__c} - ${record.name}`;
+}
+
+function logResult(rets, i) {
+  if (rets[i].success) {
+    // console.log(`#${(i + 1)} SUCCESS: sf_id = ${rets[i].id} [${instanceInfo(i)}]`);
+  } else {
+    console.log(
+      `#${i + 1} ERROR: ${rets[i].errors.join(", ")} [${instanceInfo(i)}]`
+    );
+  }
+}
+
+function batchUpsert(instances) {
+  console.log(">>> runBatch");
+  // Create job and batch
+  console.log("--> Create job and batch");
+  var job = sfConn.bulk.createJob("tulip_instance__c", "upsert", {
+    extIdField: "deployment_id__c",
+  });
+  var batch = job.createBatch();
+  // start job
+  console.log("--> start job");
+  batch.execute(instances);
+  // listen for events
+  console.log("--> listen for events");
+  batch.on("error", function (batchInfo) {
+    // fired when batch request is queued in server.
+    console.log("Error, batchInfo:", batchInfo);
+  });
+  batch.on("queue", function (batchInfo) {
+    // fired when batch request is queued in server.
+    console.log("queue, batchInfo:", batchInfo);
+    batch.poll(1000 /* interval(ms) */, 20000 /* timeout(ms) */); // start polling - Do not poll until the batch has started
+  });
+  batch.on("response", function (rets) {
+    // fired when batch finished and result retrieved
+    console.log("--> batch finished results retrieved");
+    jobResults = rets;
+    console.log("--> Results:");
+    for (var i = 0; i < rets.length; i++) {
+      logResult(rets, i);
+    }
+    console.log("--> close job, logout connection");
+    job
+      .close()
+      .then((jobInfo) => console.log("--> Job Summary <--\n", jobInfo))
+      .then((res) => sfConn.logout());
+  });
+}
+
+function saveRecords(records) {
+  orderRecords = records;
+  return orderRecords;
+}
+
+function printRecordsRaw() {
+  console.log(orderRecords);
+  return;
+}
+
+function printOrderInfo(record) {
+  console.log(
+    `${record.orderid} - ${record.orderline} - ${record.product} - ${record.qty}`
+  );
+}
+
+function printRecords() {
+  console.log("--> ORDERS <--");
+  orderRecords.forEach(printOrderInfo);
+  return;
+}
+
+//////////////////////////////
+// Main Execution
+//////////////////////////////
+
+// ${listQueryFields(FIELD_MAP)}
+// Query the product db.
+var query_text = `
+SELECT
+  *
+FROM
+corps.demands
+WHERE
+LIMIT 5
+`;
+
+console.log(query_text);
+
+function fetchInstances() {
+  return dbClient
+    .connect()
+    .then((res) => dbClient.query(query_text))
+    .then((res) => printRows(res))
+    .then((res) => saveRecords(res.rows))
+    .catch(failureCallback)
+    .then((res) => dbClient.end());
+}
+
+function runBatch() {
+  return sfConn
+    .login(sb_username, sb_password + sb_security_token)
+    .then((res) => batchUpsert(test_instances_3))
+    .catch(failureCallback);
+}
+
+function fetchAndLoad() {
+  getDBClient(SECRETS.PG_PWD, env);
+  dbClient
+    .query(query_text)
+    .then((res) => saveRecords(res.rows))
+    .then((res) => printRecordsRaw())
+    .then((res) => printRecords())
+    // .then(res => sfConn.login(sb_username, sb_password + sb_security_token)) // SANDBOX
+    //.then((res) => sfConn.login(sf_username, sf_password + sf_security_token)) // PROD
+    //.then((res) => batchUpsert(instanceRecords))
+    .catch(failureCallback)
+    .then((res) => dbClient.end());
+}
+
+// listQueryFields(FIELD_MAP);
+// fetchInstances().then(res => printRecords());
+// runBatch();
+fetchAndLoad();
